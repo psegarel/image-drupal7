1a270f641b9e6b6609d0b9fec249bb9d3de26622
psegarel <patrick@ps.dev>
2013-04-03 17:48:07 +0800

    Designed new layout, added Grape pickers illustration, big promo window & news

--- sites/all/themes/omega_image/templates/html.tpl.php
+++ sites/all/themes/omega_image/templates/html.tpl.php
@@ -7,7 +7,14 @@
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
   <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Noto+Sans:400,400italic' rel='stylesheet' type='text/css'>
+<!--
+  <link href='http://fonts.googleapis.com/css?family=Molle:400italic' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Ultra' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Comfortaa:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Dosis:400,600' rel='stylesheet' type='text/css'>
+-->
 </head>
 <body<?php print $attributes;?>>
   <div id="skip-link">
